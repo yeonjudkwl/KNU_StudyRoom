f181896822e82837a23511342756a4f26b10e0a7
yeonjudkwl <hyj9785@likelion.org>
2020-01-12 23:54:19 +0900

    nav style

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+// nav
+function openNav() {
+    document.getElementById("mySidenav").style.width = "180px";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
