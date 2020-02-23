9f63e0f9ee9c0e889faa7aabb499781e634b181d
yeonjudkwl <hyj9785@likelion.org>
2020-02-23 15:39:01 +0900

    modified scripts

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,43 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+//chart
+new Chart(document.getElementById("chart_1"),
+    {
+        "type":"doughnut",
+        "data":{
+            "datasets":[
+                {
+                    "data":[1, 2],
+                    "backgroundColor":["rgba(255, 205, 86)","rgba(79, 81, 81, .3)"],
+                    "borderWidth": 0.3
+                },
+            ]
+        }
+    });
+new Chart(document.getElementById("chart_2"),
+    {
+        "type":"doughnut",
+        "data":{
+            "datasets":[
+                {
+                    "data":[2, 1],
+                    "backgroundColor":["rgba(255, 99, 132)","rgba(79, 81, 81, .3)"],
+                    "borderWidth": 0.3
+                }
+            ]
+        }
+    });
+new Chart(document.getElementById("chart_3"),
+    {
+        "type":"doughnut",
+        "data":{
+            "datasets":[
+                {
+                    "data":[1, 3],
+                    "backgroundColor":["rgba(54, 162, 235)","rgba(79, 81, 81, .3)"],
+                    "borderWidth": 0.3
+                }
+            ]
+        }
+    });
