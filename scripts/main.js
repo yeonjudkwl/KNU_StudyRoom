fb528ccc55cbc713295fcad6c387bddfa54539aa
yeonjudkwl <hyj9785@likelion.org>
2020-03-02 21:04:41 +0900

    reserve_page | footer

--- scripts/main.js
+++ scripts/main.js
@@ -10,8 +10,11 @@ new Chart(document.getElementById("chart_1"),
     {
         "type":"doughnut",
         "data":{
+            labels: [
+                '1A',
+            ],
             "datasets":[
-                {
+                {   
                     "data":[1, 2],
                     "backgroundColor":["rgba(255, 205, 86)","rgba(79, 81, 81, .3)"],
                     "borderWidth": 0.3
@@ -23,6 +26,9 @@ new Chart(document.getElementById("chart_2"),
     {
         "type":"doughnut",
         "data":{
+            labels: [
+                '3A',
+            ],
             "datasets":[
                 {
                     "data":[2, 1],
@@ -36,6 +42,9 @@ new Chart(document.getElementById("chart_3"),
     {
         "type":"doughnut",
         "data":{
+            labels: [
+                '3B',
+            ],
             "datasets":[
                 {
                     "data":[1, 3],
